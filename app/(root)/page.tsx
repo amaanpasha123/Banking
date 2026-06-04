cff64e8c1c498c29eb8beb9f35e7fbcc25487129
Amaan Pasha <amaanpasha23@gmail.com>
2026-06-04 18:23:47 +0530

    header box started

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
- import React from 'react'
+ import HeaderBox from '@/components/HeaderBox'
+import React from 'react'
  
  const Home = () => {
+  const loggedIn = {firstname : "Amaan"}
    return (
-     <div>Home</div>
+     <section className='home'>
+      <div className='home-content'>
+          <header className='home-header'>
+            <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstname || "Guest"}
+            subtext="Access and managed your account and transaction efficiently"
+            />
+          </header>
+      </div>
+     </section>
    )
  }
  
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderBox = ({type="title", title, user, subtext}
+    :
+    HeaderBoxProps) => {
+  return (
+    <div>HeaderBox</div>
+  )
+}
+
+export default HeaderBox
