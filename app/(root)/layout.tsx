74600d4e817b61fa10f4dda34c9967fd5ebbd6a5
Amaan Pasha <amaanpasha23@gmail.com>
2026-06-10 01:45:10 +0530

    now finally we have added appwrite with Next.js now we could create the user and get all his data it is as similar as mongodb but we could controll over the authorization over here also and now we have to make sure that we will manipulate and next part will be now finally we will move forwart to make the app secure over here till now what ever we made and then we will make sure that we will add the real banking facility with it over here

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,24 @@
+
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 import Image from "next/image";
+
+
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
 
  // Expand this object with mock data to satisfy the strict 'User' type requirements
   const loggedIn = await getLoggedInUser();
+  if(!loggedIn){
+    redirect('/signin');
+  }
   return (
      <main className="flex h-screen w-full font-inter">
         <Sidebar user={loggedIn}/>
